25317bfff696154c717c20c3997f19d5523f4102
David Tierney <daveshouse44@hotmail.com>
2021-07-22 00:38:24 -0500

    Added saved local storage feature

--- Assets/script.js
+++ Assets/script.js
@@ -8,8 +8,8 @@ function displayTime() {
 var today = moment().format("dddd MMM DD, YYYY h:mma");
 $("#currentTime").html(today);
 }
-displayTime();
-//setInterval(displayTime, 1000);
+//displayTime();
+setInterval(displayTime, 1000);
 
 // This function colors rows compared to current time
 function setRowColor() {
@@ -17,8 +17,10 @@ function setRowColor() {
         currentHour = moment().format("H");
         $(".time-block").each(function () {
             var hour = parseInt($(this).attr("id"));
-            console.log(currentHour);
+            //$(this).children(".description").val(localStorage.getItem(hour)); 
+            //console.log(currentHour);
             console.log(hour);
+
             if (hour < currentHour) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -42,24 +44,21 @@ setRowColor();
 // Save user imputs to local storage
 function saveEventImput (e){
     e.preventDefault();
-    console.log("I was clicked", e.target);
-    ////// look up .each in jQuery
-    eventImput = $(this).siblings(".description").val();
+    //console.log("Saved!", e.target);
+    var eventImput = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
-    console.log("eventImput", eventImput, time);
-    //Store
+
+    // Store Event
     localStorage.setItem(time, eventImput);
-      if (eventImput === "") {
-        alert("Add an event to save to the Scheduler!");
-      }
   }
-  //On click event
+  // On click event
   $('.saveBtn').on('click', saveEventImput);
 
-  // Retrieve Imputs
-  $(".description").each(function(){
-    var hour = parseInt($(this).parent().attr("id"));
-    console.log("time", hour, "current time", currentTime); 
-    console.log("description", localStorage.getItem(hour)); 
-    $(this).children(".description").val(localStorage.getItem(hour));
-}) 
+// Keeps items displayed that are saved in local storage
+function displayEvent() {
+    $(".time-block").each(function () {
+        var hour = parseInt($(this).attr("id"));
+        $(this).children(".description").val(localStorage.getItem(hour));
+  });
+}
+displayEvent();
